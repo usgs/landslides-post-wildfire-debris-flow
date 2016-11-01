446f78247bc3d8cbb47a735cc34f2b847c6da04b
jdbrown-usgs <jdbrown@usgs.gov>
2016-11-01 13:04:28 -0600

    created navigation for post fire debris flow

---
+++ src/htdocs/_navigation.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+echo navGroup(navItem('/index.php', 'Post-fire Debris-flow Hazards'),
+    navItem("/background2016.php", 'Scientific Background') .
+    navItem("/request.php", 'Assessment Request') .
+    navItem("/disclaimer.php", 'Disclaimer') .
+    navItem("/updates.php", 'Updates')
+);
+?>
