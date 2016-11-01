ad98db8239c69f8dedd3fcac60b3a1113706ec36
jdbrown-usgs <jdbrown@usgs.gov>
2016-11-01 13:05:27 -0600

    added static content for post fire debris flow and put it in new template

---
+++ src/htdocs/updates.php
@@ -0,0 +1,28 @@
+<?php
+if (!isset($TEMPLATE)) {
+  $TITLE = "Model Updates";
+  $HEAD = '<link rel="stylesheet" href="css/index.css"/>';
+  $NAVIGATION = true;
+  $FOOT = '<script src="js/index.js"></script>';
+
+  include 'template.inc.php';
+}
+?>
+<div>
+  <p>
+    <b>September 2015</b> - The analysis methods have been updated to now make
+    calculations and output the results in the UTM NAD83 Coordinate System
+    local to the burn area.  This update serves two purposes.  First, it allows
+    dissemination of data to end-users in their native reference system.
+    Second, it allows for improved accuracy in our calculations through better
+    preservation of areas and lengths in the input data.This update is
+    effective as of 09/10/2015 and will be a part of all future model runs.
+  </p>
+  <p>
+    <b>June 2016</b> - Post-fire debris-flow likelihood estimates are now made
+    with an updated logistic regression model equation.  Additional details
+    concerning the new equation, model database, and improvements in model
+    predictions from previous iterations of the likelihood model can be found
+    here (link to OFR and/or geomorph paper).
+  </p>
+</div>
