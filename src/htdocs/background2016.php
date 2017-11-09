fc1c369e51b382f8cc3093b6a36e968de6c3e5c7
ehunter-usgs <ehunter@usgs.gov>
2017-11-09 16:14:44 -0700

    Fix markup, update equation #3 for 2016 background

--- src/htdocs/background2016.php
+++ src/htdocs/background2016.php
@@ -8,6 +8,7 @@
       include 'template.inc.php';
     }
 ?>
+
 <div>
   <p>
     The preliminary hazard assessment relies upon empirical models to
@@ -37,31 +38,43 @@
   <p>
     The likelihood of a debris-flow in response to a given peak 15-minute
     rainfall intensity are based upon a logistic regression approach, which
-    combines the following two equations: <br>
-    (1) P = e<sup>x</sup> / (1 + e<sup>x</sup>),<br> <br>Where
+    combines the following two equations:
   </p>
-  <ul>
-    <li>
-      P is the probability of debris-flow occurrence in fractional form, and
-    </li>
-    <li>
-      e<sup>x</sup> is the exponential function where e represents the
-      mathematical constant 2.718.
-    </li>
-  </ul>
+
+  <p>
+    (1) P = e<sup>x</sup> / (1 + e<sup>x</sup>)
+  </p>
+
+  <p>
+    Where
+
+    <ul>
+      <li>
+        P is the probability of debris-flow occurrence in fractional form, and
+      </li>
+      <li>
+        e<sup>x</sup> is the exponential function where e represents the
+        mathematical constant 2.718.
+      </li>
+    </ul>
+  </p>
 
   <p>
     For recently burned areas in southern California, equation 2 is used to
-    calculate x:<br/>
-
-    <br>(2) x = -3.63 + (0.41 &#215; X1R) + (0.67 &#215; X2R) + (0.7 &#215; X3R)
-
-    <br>Where
+    calculate x:
   </p>
+
+  <p>
+    (2) x = -3.63 + (0.41 &#215; X1R) + (0.67 &#215; X2R) + (0.7 &#215; X3R)
+  </p>
+
+  <p>
+    Where
+
     <ul>
       <li>
         X1R is the proportion of upslope area in burned area reflectance class
-        (BARC) Class 3 or 4 with gradients &ge;23&deg;, multiplied by the peak
+        (BARC) Class 3 or 4 with gradients &ge; 23&deg;, multiplied by the peak
         15-minute rainfall accumulation of the design storm
         (in millimeters [mm]),
       </li>
@@ -76,6 +89,8 @@
         design storm (in millimeters [mm]).
       </li>
     </ul>
+  </p>
+
   <p>
     Likelihood values predicted by the equation potentially range from 0 (least
     likely) to 1 (most likely). The predicted likelihood values are assigned to
@@ -93,29 +108,35 @@
   </p>
 
   <p>
-    Potential debris-flow volume is calculated with equation 4:<br/>
-
-    <br>(3) ln(V) = 2.89 + (0.17 &#215; sqrt(ElevRange)) +
-    (0.3 &times; ln(HM<sub>km</sub>)) + (0.47 &times; sqrt(i15))<br>
-    <br>Where
+    Potential debris-flow volume is calculated with equation 4:
   </p>
 
-  <ul>
-    <li>
-      ElevRange is the range (maximum elevation–minimum elevation) of elevation
-      values within the upstream watershed (in meters),
-    </li>
+  <p>
+    (3) ln(V) = 4.22 + (0.13 &#215; sqrt(ElevRange)) +
+    (0.36 &times; ln(HM<sub>km</sub>)) + (0.39 &times; sqrt(i15))
+  </p>
 
-    <li>
-      HM<sub>km</sub> is the area upstream of the calculation point that
-      was burned at high or moderate severity (in km<sup>2</sup>), and
-    </li>
+  <p>
+    Where
 
-    <li>
-      i15 is the spatially-averaged peak 15-min rainfall intensity for the
-      design storm in the upstream watershed (in mm/h).
-    </li>
-  </ul>
+    <ul>
+      <li>
+        ElevRange is the range (maximum elevation–minimum elevation) of elevation
+        values within the upstream watershed (in meters),
+      </li>
+
+      <li>
+        HM<sub>km</sub> is the area upstream of the calculation point that
+        was burned at high or moderate severity (in km<sup>2</sup>), and
+      </li>
+
+      <li>
+        i15 is the spatially-averaged peak 15-min rainfall intensity for the
+        design storm in the upstream watershed (in mm/h).
+      </li>
+    </ul>
+  </p>
+
   <p>
     Volume estimates were classified in order of magnitude scale ranges
     0–1,000 m<sup>3</sup>; 1,000–10,000 m<sup>3</sup>;
